d7d76d65fb27c7b37255cd29c5771409e216501c
Dayem Siddiqui <user@B-MBA-DayemSiddiqui.fritz.box>
2021-01-27 20:19:11 +0100

    feat(app.controller): expose env vars via get api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,10 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(): any {
+    return {
+      dbname: process.env.DB_NAME,
+      mongoURI: process.env.MONGO_URI,
+    };
   }
 }
