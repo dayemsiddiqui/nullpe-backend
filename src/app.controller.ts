52fd0e0acc5adab7cb85c205f2ec8070cbd6cd01
Dayem Siddiqui <user@B-MBA-DayemSiddiqui.fritz.box>
2021-01-27 21:49:21 +0100

    feat(hide): dsad

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,19 +1,10 @@
 import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
-import { ConfigService } from '@nestjs/config';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private configService: ConfigService,
-  ) {}
-
   @Get()
   getHello(): any {
     return {
-      dbname: this.configService.get<string>('database.name'),
-      mongoURI: this.configService.get<string>('database.uri'),
       status: 'fuck up',
     };
   }
