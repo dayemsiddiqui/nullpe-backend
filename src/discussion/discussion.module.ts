9fde2d928706e3d4a46405e4208f353dec13651d
Dayem Siddiqui <user@B-MBA-DayemSiddiqui.fritz.box>
2021-01-27 20:30:30 +0100

    temporarily remove mongodb

--- src/discussion/discussion.module.ts
+++ src/discussion/discussion.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { DiscussionController } from './discussion.controller';
 import { CreateDiscussionUseCase } from './usecases/CreateDiscussionUseCase';
 import { DiscussionRepository } from './repositories/DiscussionRepository';
-import { DatabaseModule } from '../shared/infra/database.module';
 
 @Module({
   controllers: [DiscussionController],
   providers: [DiscussionRepository, CreateDiscussionUseCase],
-  imports: [DatabaseModule],
+  imports: [],
 })
 export class DiscussionModule {}
